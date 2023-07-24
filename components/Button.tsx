94712ffb34213cad26aad8f6b85062b3d91cd46e
lguifcosta <lgcosta.lgfc@gmail.com>
2023-07-24 14:44:53 -0300

    front setup

---
+++ components/Button.tsx
@@ -0,0 +1,40 @@
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps 
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement , ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type = "button",
+    ...props
+} ,  ref)=>{
+    return(
+    <button type={type}
+    className={twMerge(`
+    w-full
+    rounded-full
+    bg-green-500
+    border-transparent
+    px-3
+    py3
+    disabled:cursor-not-allowed
+    disabled:opacity-75
+    font-bold
+    hover:opacity-75
+    transmition
+    `, className )}
+    disabled={disabled}
+    ref={ref}
+    {...props}
+    >
+        {children}
+    </button>
+    )
+})
+   
+Button.displayName = "Button"
+
+export default Button;
