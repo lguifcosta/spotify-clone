3d91ae1bb35545acf960e53535aedb432e5fca9e
lguifcosta <lgcosta.lgfc@gmail.com>
2023-07-26 16:34:40 -0300

    added the providers components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
 
 const inter = Figtree({ subsets: ['latin'] })
 
@@ -18,9 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <SupabaseProvider>
+        <UserProvider>
         <Sidebar>
         {children}
         </Sidebar>
+        </UserProvider>
+        </SupabaseProvider>
         </body>
     </html>
   )
